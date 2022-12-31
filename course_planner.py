d6abc82a1c8de853653a1bdcf4934027045f635e
kathireshng <kathireshng@gmail.com>
2022-12-31 14:31:11 +1000

    Incompatibility checks inserted in Semester class.
    Semester class can now check that none of its classes are incompatible with each other. This functionality is intended to be used with minimal extension in Plan class.

--- course_planner.py
+++ course_planner.py
@@ -10,24 +10,25 @@ class Course:
         self.prereq = self.courseInfo[code][PREREQ]
         self.incomp = self.courseInfo[code][INCOMP]
     
-    def is_incompatible_with(self, course: 'Course') -> bool:
-        return course.get_code() in self.incomp
-
     def get_code(self) -> str:
         return self.code
 
-    def get_prerequisites(self):
+    def get_prerequisites(self) -> list[list[str]]:
         return self.prereq
 
     def get_incompatible(self) -> list[str]:
         return self.incomp
+
+    def is_incompatible_with(self, course: 'Course') -> bool:
+        return course.get_code() in self.incomp
 
     def __str__(self) -> str:
         pass
 
 
 class Semester:
-    def __init__(self, name, course1, course2, course3=None, course4=None) -> None:
+    def __init__(self, name, course1: Course, course2: Course, 
+                 course3=None, course4=None) -> None:
         self.name = name
         self.courses = list(course1, course2)
         if course3:
@@ -39,11 +40,32 @@ class Semester:
             print(ERR_MSG_TOO_MANY_COURSES)
             self.courses = list(course1, course2)
 
+    def get_courses(self) -> list[Course]:
+        return self.courses
+
+    def get_course_names(self) -> list[str]:
+        name_list = []
+        for course in self.get_courses():
+            name_list.append(course.get_code())
+        return name_list
+
     def is_summer_sem(self) -> bool:
         return self.name % 3 == 0
 
+    def _get_all_incompatiblities(self) -> list[str]:
+        incomp_list = []
+        for course in self.get_courses():
+            incomp_list.append(course.get_incompatible())
+        return incomp_list
 
-class Plan:
+    def any_incompatibilites(self) -> bool:
+        for course in self._get_all_incompatiblities():
+            if course in self.get_course_names():
+                return True
+        return False
+
+
+class Plan: #TODO Implement method or methods to check that prerequisistes have been met.
     def __init__(self) -> None:
         pass
 
