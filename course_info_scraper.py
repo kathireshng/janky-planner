d53b6dada8220348053141ece8394c404ed94c7a
kathireshng <kathireshng@gmail.com>
2023-07-16 12:13:48 +1000

    Added get_course_codes_from_webpage function to scrape all courses listed on UQ engineering course list webpage.

--- course_info_scraper.py
+++ course_info_scraper.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from pathlib import Path
 from os import mkdir
 
-
+BENGG_COURSE_LIST = 'https://my.uq.edu.au/programs-courses/requirements/program/2455/2021'
 
 def scrape_course_names(url: str) -> list[str]:
     browser = webdriver.Safari()
@@ -33,8 +33,25 @@ def get_course_info(course_list: list[str], dest_dir=COURSE_DIR_STR):
         download_course_info(course_code, course_dir, browser)
     browser.close()
 
+def get_course_codes_from_webpage(url: str):
+    browser = webdriver.Safari()
+    browser.minimize_window()
+    browser.get(url)
+    text = browser.page_source
+    browser.close()
+    course_codes = []
+    for i in range(len(text)):
+        candidate_code = text[i:i+8]
+        if candidate_code in course_codes:
+            continue
+        if candidate_code[0:4].isalpha() and candidate_code[4:8].isnumeric():
+            course_codes.append(text[i:i+8].upper())
+    course_codes.sort()
+    for course_code in course_codes:
+        print(course_code)
+
 def main():
-    pyperclip.copy(URL_BASE)
+    get_course_codes_from_webpage(BENGG_COURSE_LIST)
 
 if __name__ == '__main__':
     main()
